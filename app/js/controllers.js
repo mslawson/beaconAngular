d6ee1083bf096c4bc812043cb31f59cad6052198
Matt Slawson <mdslawson@gmail.com>
2014-09-12 14:46:45 -0500

    updating nav bar - early styling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,54 +8,80 @@ jobcostApp.controller('JobListCtrl', function ($scope) {
 	{'jobName': 'Village at Lionsgate',
 	'fileNumber': '414-10-1600',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-50',
 	'totalPrice': 4071},
+
 	{'jobName': 'Creekwood Commons',
 	'fileNumber': '441-10-1633',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-50',
 	'totalPrice': 1},
+
 	{'jobName': 'BOA Raytown',
 	'fileNumber': '472-11-1816',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-50',
 	'totalPrice':300},
+	
 	{'jobName': 'Village at Lionsgate',
 	'fileNumber': '414-10-1600',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-60',
 	'totalPrice': 4071},
+	
 	{'jobName': 'Creekwood Commons',
 	'fileNumber': '441-10-1633',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-70',
 	'totalPrice': 5},
+
 	{'jobName': 'BOA Raytown',
 	'fileNumber': '472-11-1816',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-70',
 	'totalPrice':100},
+
 	{'jobName': 'Village at Lionsgate',
 	'fileNumber': '414-10-1600',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-60',
 	'totalPrice': 4071},
+
 	{'jobName': 'Creekwood Commons',
 	'fileNumber': '441-10-1633',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-50',
 	'totalPrice': 8},
+
 	{'jobName': 'BOA Raytown',
 	'fileNumber': '472-11-1816',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-60',
 	'totalPrice':200},
+
 	{'jobName': 'Village at Lionsgate',
 	'fileNumber': '414-10-1600',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-70',
 	'totalPrice': 4071},
+
 	{'jobName': 'Creekwood Commons',
 	'fileNumber': '441-10-1633',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-70',
 	'totalPrice': 8},
+
 	{'jobName': 'BOA Raytown',
 	'fileNumber': '472-11-1816',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-60',
 	'totalPrice':200},
+
 	{'jobName': 'Parkhill Offices',
 	'fileNumber': '520-11-2109',
 	'billingDate': '08/10/2010',
+	'jobCode': '5-50',
+	'budgetHrs': 80,
 	'totalPrice':10}
 	];
 	$scope.orderProp = 'jobName';
@@ -66,6 +92,14 @@ jobcostApp.controller('JobListCtrl', function ($scope) {
 			summedTotalPrices = summedTotalPrices + job.totalPrice;
 		});
 		return jobs.summedTotalPrices = summedTotalPrices;
+	}
+
+	$scope.getSumBudgetHrs = function(jobs){
+		var summedBudgetHrs = 0;
+		jobs.forEach(function (job){
+			summedBudgetHrs = summedBudgetHrs + job.budgetHrs;
+		});
+		return jobs.summedBudgetHrs = summedBudgetHrs;
 	}
 
 	

5282552974b6d57644bc82c087730b187428da65
Matt Slawson <mdslawson@gmail.com>
2014-10-04 12:31:41 -0500

    Created job list partial and reworked index - also updated bower

--- beacon-app/js/app.js
+++ beacon-app/js/app.js
@@ -2,6 +2,22 @@
 
 /* App Module */
 var jobcostApp = angular.module('jobcostApp', [
-// 'ngRoute',
+'ngRoute',
 'jobcostControllers'
 ]);
+
+jobcostApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/jobs', {
+        templateUrl: 'partials/job-list.html',
+        controller: 'JobListCtrl'
+      }).
+      when('/jobs/:jobName', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/jobs'
+      });
+  }]);

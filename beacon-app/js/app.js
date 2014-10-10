5868f921f04eb475b286a66a16c9d2a67db59641
Matt Slawson <mdslawson@gmail.com>
2014-10-10 15:57:47 -0500

    Updated sidelist with yds per hour and modified job css

--- beacon-app/js/app.js
+++ beacon-app/js/app.js
@@ -21,3 +21,17 @@ jobcostApp.config(['$routeProvider',
         redirectTo: '/jobs'
       });
   }]);
+
+jobcostApp.controller("PanelController", function(){
+  this.tab = 1;
+
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab) {
+    return this.tab === checkTab;
+  };
+
+
+});
+
--- beacon-app/js/controllers.js
+++ beacon-app/js/controllers.js
@@ -3,6 +3,7 @@
 /* Controllers */
 var jobcostControllers = angular.module('jobcostControllers', []);
 
+
 jobcostControllers.controller('JobListCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('jobs/jobs.json').success(function(data) {
@@ -17,7 +18,7 @@ jobcostControllers.controller('JobListCtrl', ['$scope', '$http',
 				summedTotalPrices = summedTotalPrices + job.totalPrice;
 			});
 			return jobs.summedTotalPrices = summedTotalPrices;
-		}
+		};
 
 		$scope.getSumBudgetHrs = function(jobs){
 			var summedBudgetHrs = 0;
@@ -25,7 +26,17 @@ jobcostControllers.controller('JobListCtrl', ['$scope', '$http',
 				summedBudgetHrs = summedBudgetHrs + job.budgetHrs;
 			});
 			return jobs.summedBudgetHrs = summedBudgetHrs;
-		}
+		};
+
+    $scope.getBudYardsPerHr = function(jobs){
+      var budYardsHr = 0;
+      var jobCount = 0;
+      jobs.forEach(function (job){
+        budYardsHr = budYardsHr + (job.budYds/job.budgetHrs);
+        jobCount = jobCount + 1;
+      });
+      return budYardsHr/jobCount;
+    };
 }]);
 
 jobcostControllers.controller('JobDetailCtrl', ['$scope', '$routeParams', '$http',
@@ -39,6 +50,11 @@ jobcostControllers.controller('JobDetailCtrl', ['$scope', '$routeParams', '$http
     	pricePer = job.totalPrice / job.budUnits;
     	return job.pricePer = pricePer;
     }
+
+    $scope.getBudYardsPerHr = function(job){
+      var budYardsHr = job.budYds / job.budgetHrs;
+      return budYardsHr;
+    };
 }]);
 
 

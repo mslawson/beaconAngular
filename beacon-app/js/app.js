15a2b19bf202ec46c04d6b6af4e2f6dc846dcbdd
Matt Slawson <mdslawson@gmail.com>
2014-10-23 08:47:03 -0500

    Created Creekwood Commons job and cleaned up navigation

--- beacon-app/js/app.js
+++ beacon-app/js/app.js
@@ -22,16 +22,5 @@ jobcostApp.config(['$routeProvider',
       });
   }]);
 
-jobcostApp.controller("PanelController", function(){
-  this.tab = 1;
 
-  this.selectTab = function(setTab) {
-    this.tab = setTab;
-  };
-  this.isSelected = function(checkTab) {
-    return this.tab === checkTab;
-  };
-
-
-});
 
--- beacon-app/js/controllers.js
+++ beacon-app/js/controllers.js
@@ -1,10 +1,31 @@
 'use strict';
 
 /* Controllers */
-var jobcostControllers = angular.module('jobcostControllers', []);
+var jobcostApp = angular.module('jobcostControllers', []);
 
+jobcostApp.controller("PanelController", function(){
+  this.tab = 1;
 
-jobcostControllers.controller('JobListCtrl', ['$scope', '$http',
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab) {
+    return this.tab === checkTab;
+  };
+
+});
+
+jobcostApp.controller("JobFormController", function(){
+  this.job = {};
+
+  this.addJob = function(jobs){
+    jobs.push(this.job);
+    this.job = {};  
+  };
+
+});
+
+jobcostApp.controller('JobListCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('jobs/jobs.json').success(function(data) {
       $scope.jobs = data;
@@ -39,7 +60,7 @@ jobcostControllers.controller('JobListCtrl', ['$scope', '$http',
     };
 }]);
 
-jobcostControllers.controller('JobDetailCtrl', ['$scope', '$routeParams', '$http',
+jobcostApp.controller('JobDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
     $http.get('jobs/' + $routeParams.jobId + '.json').success(function(data) {
       $scope.job = data;
